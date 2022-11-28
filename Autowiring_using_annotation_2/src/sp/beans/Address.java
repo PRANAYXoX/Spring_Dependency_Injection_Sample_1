a87b6c0237b84acb6fae9d9aa2fd76ce137a6163
pranayXoX <pranay.dmnsns@gmail.com>
2022-11-28 10:06:45 +0530

    @Qualifier based Autowiring...

---
+++ Autowiring_using_annotation_2/src/sp/beans/Address.java
@@ -0,0 +1,29 @@
+package sp.beans;
+
+public class Address {
+		
+	private int houseno;
+	private String city;
+	private String street;
+	
+	public int getHouseno() {
+		return houseno;
+	}
+	public void setHouseno(int houseno) {
+		this.houseno = houseno;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	
+}
---
+++ Autowiring_using_annotation_2/src/sp/beans/Student.java
@@ -0,0 +1,42 @@
+package sp.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+
+public class Student {
+		private String name;
+		private int roll;
+		private Address address;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getRoll() {
+			return roll;
+		}
+		public void setRoll(int roll) {
+			this.roll = roll;
+		}
+		public Address getAddress() {
+			return address;
+		}
+		
+		//@Required
+		@Autowired(required = true)
+		@Qualifier("address2")
+		public void setAddress(Address address) {
+			this.address = address;
+		}
+		
+		public void display() {
+			System.out.print("\nNAME: "+name);
+			System.out.print("\nROLL: "+roll);
+			System.out.print("\nAddress:"+address.getHouseno()+", "+address.getCity()+", "+address.getStreet());
+		}
+		
+		
+}
